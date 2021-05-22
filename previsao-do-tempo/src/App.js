b32e69f43fec1fdde0aded75642cefa78c240566
Aryadne Bastos <49688772+aryadnebastos@users.noreply.github.com>
2021-05-22 19:33:45 -0300

    ajustes

--- previsao-do-tempo/src/App.js
+++ previsao-do-tempo/src/App.js
@@ -1,59 +1,64 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-const api = 'https://api.hgbrasil.com/weather?woeid=12578840&format=json-cors&locale=pt';
+const api =
+  "https://api.hgbrasil.com/weather?woeid=12578840&format=json-cors&key=ee4d74a8&locale=pt";
 export default class App extends React.Component {
-state = {
-  city: "",
-  forecast: [],
-};
+  state = {
+    city: "",
+    forecast: [],
+  };
 
-componentDidMount(){
-  axios.get(api)
-  .then(({ data }) => {
-    this.setState({
-      city: data.results.city_name,
-      forecast: data.results.forecast
-    });
-  });
-
-}
-render() {
-  return (
-    <div className = "container">
-      <h1> {this.state.city} </h1>
-      <table className = "striped centered">
-        <thead> 
-          <tr>
-            <th>Data </th>
-            <th>Min. </th>
-            <th>Max. </th>
-            <th>Previsão  </th>
-            <th>Imagem</th>
-          </tr>
-          
-        </thead>
-        <tbody>
-          {
-            this.state.forecast.map(day => {
-              return(
-              <tr key = {day.date}>
-                <td>{day.date}</td>
-                <td>{day.min}</td>
-                <td>{day.max}</td>
-                <td>{day.description}</td>
-                <td> <img 
-                  src={`/weather-icons/${day.condition}.svg`}
-                  alt={day.description} /> 
-                </td>
-              </tr>
+  componentDidMount() {
+    axios
+      .get(api, {
+        crossdomain: true,
+        headers: {
+         'Content-Type': 'application/json'
+        }
+      })
+      .then(({ data }) => {
+        this.setState({
+          city: data.results.city_name,
+          forecast: data.results.forecast,
+        });
+      });
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1> {this.state.city} </h1>
+        <table className="striped centered">
+          <thead>
+            <tr>
+              <th>Data </th>
+              <th>Min. </th>
+              <th>Max. </th>
+              <th>Previsão </th>
+              <th>Imagem</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.forecast.map((day) => {
+              return (
+                <tr key={day.date}>
+                  <td>{day.date}</td>
+                  <td>{day.min}</td>
+                  <td>{day.max}</td>
+                  <td>{day.description}</td>
+                  <td>
+                    {" "}
+                    <img
+                      src={`/weather-icons/${day.condition}.svg`}
+                      alt={day.description}
+                    />
+                  </td>
+                </tr>
               );
-            })
-          } 
-        </tbody>
-      </table>
-    </div>
-  );
-}
-
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
