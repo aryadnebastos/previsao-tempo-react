ccdfa9e7c391aeb3583f9b05984ab7e42dec4192
Aryadne Bastos <49688772+aryadnebastos@users.noreply.github.com>
2021-05-22 17:38:20 -0300

    conclusão

--- previsao-do-tempo/src/App.js
+++ previsao-do-tempo/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
+const api = 'https://api.hgbrasil.com/weather?woeid=12578840&format=json-cors&locale=pt';
+//const busca = 'https://api.hgbrasil.com/stats/find_woeid?key=17284dd0&format=json-cors&sdk_version=console&city_name=';
+export default class App extends React.Component {
+state = {
+  city: "",
+  forecast: [],
+};
+
+componentDidMount(){
+  axios.get(api)
+  .then(({ data }) => {
+    this.setState({
+      city: data.results.city_name,
+      forecast: data.results.forecast
+    });
+  });
+/*   fetch(api)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      this.setState({
+        city: json.results.city_name
+      });
+    }); */
+}
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "container">
+      <h1> {this.state.city} </h1>
+      <table className = "striped centered">
+        <thead> 
+          <tr>
+            <th>Data </th>
+            <th>Min. </th>
+            <th>Max. </th>
+            <th>Previsão  </th>
+            <th>Imagem</th>
+          </tr>
+          
+        </thead>
+        <tbody>
+          {
+            this.state.forecast.map(day => {
+              return(
+              <tr key = {day.date}>
+                <td>{day.date}</td>
+                <td>{day.min}</td>
+                <td>{day.max}</td>
+                <td>{day.description}</td>
+                <td> <img 
+                  src={`/weather-icons/${day.condition}.svg`}
+                  alt={day.description} /> 
+                </td>
+              </tr>
+              );
+            })
+          } 
+        </tbody>
+      </table>
     </div>
   );
 }
 
-export default App;
+}
+
+//export default App;
